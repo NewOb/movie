e81e4264817d1f23c309723315b37e53171f5578
vr <854306349@qq.com>
2018-07-13 19:26:47 +0800

    词云图插件引入

--- IMDB.py
+++ IMDB.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template,request, jsonify,url_for,redirect
+from flask import Flask, render_template, request, jsonify, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 import config
-from  models import xunlian
+from models import xunlian
 from exts import db
 
 app = Flask(__name__)
@@ -9,24 +9,92 @@ app.config.from_object(config)
 db.init_app(app)
 
 
-@app.route('/data/',methods=['GET'])
+@app.route('/data/', methods=['GET'])
 def data():
-    ALL=xunlian.query.filter(xunlian.country=='USA').all()
-    all_data={}
-    ALLDATA=[]
-    while len(ALL)>0:
-        all_xl=ALL.pop()
-        all_data['NAME']=all_xl.name
-        ALLDATA.append(all_data)
-        all_data={}
+    d_type = [{'en': 'Comedy', 'zh': '喜剧','like':0},
+              {'en': 'Adventure', 'zh': '冒险','like':0},
+              {'en': 'Fantasy', 'zh': '幻想','like':0},
+              {'en': 'Mystery', 'zh': '悬念','like':0},
+              {'en': 'Thriller', 'zh': '惊悚','like':0},
+              {'en': 'Documentary', 'zh': '记录','like':0},
+              {'en': 'War', 'zh': '战争','like':0},
+              {'en': 'Western', 'zh': '西部','like':0},
+              {'en': 'Romance', 'zh': '爱情','like':0},
+              {'en': 'Drama', 'zh': '剧情','like':0},
+              {'en': 'Horror', 'zh': '恐怖','like':0},
+              {'en': 'Action', 'zh': '动作','like':0},
+              {'en': 'Sci-Fi', 'zh': '科幻','like':0},
+              {'en': 'Music', 'zh': '音乐','like':0},
+              {'en': 'Family', 'zh': '家庭','like':0},
+              {'en': 'Crime', 'zh': '犯罪','like':0},
+              ]
+    # com = adv = fan = mys = thr = doc = war = wes = rom = dra = hor = act = sci = mus = fam = cri = 0
+    # mytype = []
+    ALL = xunlian.query.all()
+    while len(ALL) > 0:
+        dataset = ALL.pop()
+        types = dataset.type.split("|")
+        like = dataset.like_all
+        if like == '':
+            like = 0
+        # print(like)
+        like = int(like)
+        # print(type(like))
+        # print(repr(like))
+        while len(types):
+            count = types.pop()
+            if count == d_type[0]["en"]:
+                d_type[0]["like"] += like
+            elif count == d_type[1]["en"]:
+                d_type[1]["like"] += like
+            elif count == d_type[2]["en"]:
+                d_type[2]["like"] += like
+            elif count == d_type[3]["en"]:
+                d_type[3]["like"] += like
+            elif count == d_type[4]["en"]:
+                d_type[4]["like"] += like
+            elif count == d_type[5]["en"]:
+                d_type[5]["like"] += like
+            elif count == d_type[6]["en"]:
+                d_type[6]["like"] += like
+            elif count == d_type[7]["en"]:
+                d_type[7]["like"] += like
+            elif count == d_type[8]["en"]:
+                d_type[8]["like"] += like
+            elif count == d_type[9]["en"]:
+                d_type[9]["like"] += like
+            elif count == d_type[10]["en"]:
+                d_type[10]["like"] += like
+            elif count == d_type[11]["en"]:
+                d_type[11]["like"] += like
+            elif count == d_type[12]["en"]:
+                d_type[12]["like"] += like
+            elif count == d_type[13]["en"]:
+                d_type[13]["like"] += like
+            elif count == d_type[14]["en"]:
+                d_type[14]["like"] += like
+            elif count == d_type[15]["en"]:
+                d_type[15]["like"] += like
 
-    return jsonify(ALLDATA)
+    # d_type[0]["like"] = com
+    # fin = xunlian.query.all()
+    # FinCloud = []
+    #
+    # while len(fin) > 0:
+    #     clouds = fin.pop()
+    #     d_type['type'] = clouds.type
+    #     FinCloud.append(cloud)
+    #     all_data = {}
 
-@app.route('/chart/',methods=['GET'])
+    return jsonify(d_type)
+
+
+@app.route('/chart/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def tabs():
     if request.method == 'GET':
         return render_template('tabs.html')
@@ -39,8 +107,16 @@ def tabs():
         key_word = request.form.get('key_words')
         CBW = request.form.get('CBW')
         level = request.form.get('level')
+        print(name)
+        print(director)
+        print(act)
+        print(type)
+        print(invest)
+        print(key_word)
+        print(CBW)
         print(level)
         return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,58 @@
+var fill = d3.scale.category20();
 
+var layout = d3.layout.cloud()
+    .size([500, 500])  // 宽高
+    .words([
+        "Hello", "world", "normally", "you", "want", "more", "words",
+        "than", "this"].map(function (d) {
+        return {text: d, size: 10 + Math.random() * 90};
+    }))  // 数据
+    .padding(5)  // 内间距
+    .rotate(function () {
+        return ~~(Math.random() * 2) * 90;
+    })
+    .font("Impact")
+    .fontSize(function (d) {
+        return d.size;
+    })
+    .on("end", draw);
 
+layout.start();
 
-d3.json("/data/",function(error,tabs)
-            {
-                if(error)
-                    console.log(error);
-                console.log(tabs);
-            }
-        );
+// 渲染
+function draw(words) {
+    d3.select("#cloud").append("svg")
+        .attr("width", layout.size()[0])
+        .attr("height", layout.size()[1])
+        .append("g")
+        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function (d) {
+            return d.size + "px";
+        })
+        .style("font-family", "Impact")
+        .style("fill", function (d, i) {
+            return fill(i);
+        })
+        .attr("text-anchor", "middle")
+        .attr("transform", function (d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function (d) {
+            return d.text;
+        });
+}
+
+d3.json("/data/", function (error, tabs) {
+        var i = 0;
+        if (error)
+            console.log(error);
+        console.log(tabs);
+        while (tabs) {
+            console.log(tabs[i].zh);
+            i++;
+        }
+    }
+);
