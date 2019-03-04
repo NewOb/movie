b2f93999f11a65b60267a45b1572df47e6276896
虚拟现实实验室 <854306349@qq.com>
2019-03-04 22:08:16 +0800

    rect

--- static/js/rect.js
+++ static/js/rect.js
@@ -3,8 +3,9 @@ height = 420;
 var data1 = [100000, 100000, 100000, 100000];
 var data2 = [20000, 60000, 40000, 90000];
 var data3 = [50000, 50000, 50000, 50000];
-var name = ["导演", "主演1", "主演2", "主演3"];
+var names = ["导演", "主演1", "主演2", "主演3"];
 
+console.log(names[0]);
 var svg = d3.select("#bullet")
     .append("svg")
     .attr("id", "bsvg")
@@ -12,7 +13,7 @@ var svg = d3.select("#bullet")
     .attr("height", height);
 
 var yScale = d3.scale.ordinal()
-    .domain(name)
+    .domain(names)
     .rangeRoundBands([0, height - 20]);
 
 // var xAxis = d3.svg.axis()
@@ -23,38 +24,74 @@ var yAxis = d3.svg.axis()
     .scale(yScale)
     .orient("left");
 
-// svg.append("g")
-//     .attr("class",baxis)
-//     .call(yAxis);
-
-svg.selectAll(".lbar")
+var lbar = svg.selectAll(".lbar")
     .data(data1)
     .enter()
     .append("rect")
     .attr("class", "lbar")
     .attr("y", function (d, i) {
-        return yScale(name[i])
+        return yScale(names[i])
     })
-    .attr("transform","translate(110,30)")
+    .attr("transform", "translate(110,30)")
     .attr("fill", "#EB9FA0")
-    .attr("rx",20)
-    .attr("ry",20)
+    .attr("rx", 20)
+    .attr("ry", 20)
     .attr("width", 500)
     .attr("height", 40);
 
-svg.selectAll(".zbar")
+var zbar = svg.selectAll(".zbar")
     .data(data2)
     .enter()
     .append("rect")
     .attr("class", "zbar")
     .attr("y", function (d, i) {
-        return yScale(name[i])
+        return yScale(names[i])
     })
     .attr("fill", "#EB586F")
-    .attr("width", function (d,i) {
-        return (d/data1[i])*500;
+    .attr("width", function (d, i) {
+        return (d / data1[i]) * 500;
     })
-    .attr("rx",20)
-    .attr("ry",20)
-    .attr("transform","translate(110,30)")
+    .attr("rx", 20)
+    .attr("ry", 20)
+    .attr("transform", "translate(110,30)")
     .attr("height", 40);
+
+var qbar = svg.selectAll(".qbar")
+    .data(data3)
+    .enter()
+    .append("rect")
+    .attr("class", "qbar")
+    .attr("y", function (d, i) {
+        return yScale(names[i])
+    })
+    .attr("fill", "#2e2e2e")
+    .attr("width",10)
+    .attr("rx", 5)
+    .attr("ry", 5)
+    .attr("transform", function (d,i) {
+        var result = (d/data1[i])*500;
+        return "translate("+result+",30)"
+    })
+    .attr("height", 40);
+
+svg.selectAll(".rtext")
+.data(names)
+.enter()
+.append("text")
+.attr("class","rtext")
+.attr("fill","#F8FFF7")
+.text(function (d) {
+    return d;
+})
+    .attr("y", function (d) {
+        return yScale(d)
+    })
+    .attr("font-size",30)
+.attr("transform","translate(20,60)");
+
+
+//zbar的宽度需要改变
+//qbat的translate改变
+function rup() {
+
+}
